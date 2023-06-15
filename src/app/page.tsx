4a21e9f5c43d165e8f67004994e79aaf7b36184c
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-06-14 20:53:58 -0700

    added button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { Inter } from "next/font/google";
 import { Calendar } from "@/components/ui/calendar";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return <Calendar />;
